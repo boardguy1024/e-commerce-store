1ec7311f680e9506da08a9ddfd42a914a48df31b
park kyung suk <boardguy1024@gmail.com>
2019-09-29 14:26:14 +0900

    add directory section(menu-item) and finished styling these

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+import "./directory.style.scss";
+import { MenuItem } from "../menu-item/menu-item.component";
+
+class Directory extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          id: 1,
+          title: "hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
+        },
+        {
+          id: 2,
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
+        },
+        {
+          id: 3,
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
+        },
+        {
+          id: 4,
+          title: "womens",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          size: "large"
+        },
+        {
+          id: 5,
+          title: "mens",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          size: "large"
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(section => (
+          <MenuItem key={section.id} props={section} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
